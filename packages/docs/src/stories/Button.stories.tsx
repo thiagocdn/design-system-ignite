f86e951aed9f10adb24c95cee94b520f883dd488
Thiago Cedran Santos <thiago.cedran@doji.co>
2022-11-12 19:34:11 -0300

    Starting Repo

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,18 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Button, ButtonProps } from '@ignite-ui-thiagocdn/react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Enviar',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big',
+  },
+}
