4e3c4c2f8a429c26917fb26582a7d8aa98738077
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-11-15 17:42:34 -0300

    Fixed path on storybook deploy

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -15,5 +15,10 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/design-system-ignite'
+    }
   }
 }
